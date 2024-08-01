57e9c0a0a2abef8f97f7bc74a753207ac0620811
daniel99estrada <pildoestrada@gmail.com>
2024-08-01 16:30:06 -0400

    docker compose setup for fron end  and backend

--- backend/file_processing.py
+++
@@ -1,8 +0,0 @@
-# file_processing.py
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
-
-def modify_file(content):
-    # Example modification: Convert content to uppercase
-    return content.upper()
